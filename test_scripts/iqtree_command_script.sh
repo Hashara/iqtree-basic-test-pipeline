2f5dddeeea7ba67d9163b86035da17f4c8bfe06b
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2024-08-05 16:52:30 +1000

    bug fix

--- test_scripts/iqtree_command_script.sh
+++ test_scripts/iqtree_command_script.sh
@@ -2,7 +2,7 @@
 ######## input varibles #################
 export WORK_DIR="/scratch/dx61/sa0557/iqtree2/ci-cd"
 source ${WORK_DIR}/helpers/create_env.sh
-source ${WORK_DIR}/helpers/parse_input.sh
+source ${WORK_DIR}/helpers/env.sh
 
 ncpus=$ARG1
 nthreads=$ARG2
